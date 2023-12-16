0adc357da40f698fa17ad295acf5d210188256d3
motailab <mdmotailab@gmail.com>
2023-12-16 19:01:38 +0600

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,155 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import { Avatar, Box, Button, InputLabel, Tab, Tabs, TextField } from '@mui/material'
+import { useState } from 'react'
+import Medallions from './medallions'
+
+interface TabPanelProps {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  }
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  )
+}
+
+const Home = () => {
+  const [value, setValue] = useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardContent style={{ boxShadow: 'none' }}>
+            <Box display={'flex'} justifyContent={'space-between'} alignItems={'flex-start'}>
+              <Box flexDirection={'row'} display={'flex'} gap={5} alignItems={'center'}>
+                <Avatar sx={{ width: 192, height: 192 }} />
+                <Box>
+                  <Typography fontWeight={'bold'} fontSize={22} mb={2}>
+                    MD Tuhin
+                  </Typography>
+                  <Box display={'flex'} gap={4} mb={1}>
+                    <Typography fontWeight={'semibold'}>Email:</Typography>
+                    <Typography>tauhidulislam100@gmail.com</Typography>
+                  </Box>
+                  <Box display={'flex'} gap={4}>
+                    <Typography fontWeight={'semibold'}>Joined:</Typography>
+                    <Typography>calendar Joined about 17 hours ago</Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Box
+                sx={{
+                  background: '#f7ecd4',
+                  border: '1px solid #d7a229',
+                  borderRadius: '8px',
+                  padding: '20px',
+                  width: '346px'
+                }}
+              >
+                <Typography mb={'4px'}>Welcome back!</Typography>
+                <Typography>
+                  Thank you for choosing Turning Hearts to keep the memory of your loved ones alive. We are honored to
+                  be a part of your healing journey.
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={value} onChange={handleChange} aria-label='basic tabs example' centered>
+                <Tab label='My Favorites' {...a11yProps(0)} />
+                <Tab label='Post' {...a11yProps(1)} />
+                <Tab label='Medallions' {...a11yProps(2)} />
+                <Tab label='My account' {...a11yProps(3)} />
+              </Tabs>
+            </Box>
+            <CustomTabPanel value={value} index={0}>
+              <Typography>My Favorites</Typography>
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              <Typography>Post</Typography>
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+              <Medallions />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={3}>
+              <Typography fontWeight={600} fontSize={16}>
+                Edit Profile
+              </Typography>
+              <Box display={'flex'} justifyContent={'flex-end'}>
+                <Button size='large' type='submit' variant='contained'>
+                  Change Password
+                </Button>
+              </Box>
+              <Box sx={{ borderBottom: 1, borderColor: 'black', paddingBottom: '10px', mb: '32px' }}>
+                <Typography>Personal Details</Typography>
+              </Box>
+              <Box flexDirection={'row'} display={'flex'} gap={10} my={'10px'}>
+                <Box flex={1}>
+                  <InputLabel htmlFor='firstName'>First Name:</InputLabel>
+                  <TextField autoFocus fullWidth id='firstName' placeholder='First Name' size='small' />
+                </Box>
+                <Box flex={1}>
+                  <InputLabel htmlFor='lastName'>Last Name:</InputLabel>
+                  <TextField autoFocus fullWidth id='lastName' placeholder='Last Name' size='small' />
+                </Box>
+              </Box>
+              <Box sx={{ borderBottom: 1, borderColor: 'black', paddingBottom: '10px', my: '32px' }}>
+                <Typography>Location Details</Typography>
+              </Box>
+              <Box flexDirection={'row'} display={'flex'} gap={10} my={'10px'}>
+                <Box flex={1}>
+                  <InputLabel htmlFor='state'>State:</InputLabel>
+                  <TextField autoFocus fullWidth id='state' placeholder='State' size='small' />
+                </Box>
+                <Box flex={1}>
+                  <InputLabel htmlFor='city'>City:</InputLabel>
+                  <TextField autoFocus fullWidth id='city' placeholder='City' size='small' />
+                </Box>
+              </Box>
+              <Box display={'flex'} justifyContent={'flex-end'} sx={{ mt: 10 }}>
+                <Button size='large' type='submit' variant='contained' color='secondary'>
+                  Save Changes
+                </Button>
+              </Box>
+            </CustomTabPanel>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Home
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,185 @@
+// ** React Imports
+import { ChangeEvent, ReactNode, useState } from 'react'
+
+// ** Next Import
+import Link from 'next/link'
+
+// ** MUI Components
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Checkbox from '@mui/material/Checkbox'
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import CardContent from '@mui/material/CardContent'
+import FormControl from '@mui/material/FormControl'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import { styled, useTheme } from '@mui/material/styles'
+import MuiCard, { CardProps } from '@mui/material/Card'
+import InputAdornment from '@mui/material/InputAdornment'
+import MuiFormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Configs
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Layout Import
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+import { useAuth } from 'src/hooks/useAuth'
+
+interface State {
+  password: string
+  email: string
+  showPassword: boolean
+  rememberMe: boolean
+}
+
+// ** Styled Components
+const Card = styled(MuiCard)<CardProps>(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: { width: 450 }
+}))
+
+const FormControlLabel = styled(MuiFormControlLabel)<FormControlLabelProps>(({ theme }) => ({
+  '& .MuiFormControlLabel-label': {
+    fontSize: '0.875rem',
+    color: theme.palette.text.secondary
+  }
+}))
+
+const LoginV1 = () => {
+  const { login } = useAuth()
+  // ** State
+  const [values, setValues] = useState<State>({
+    password: '',
+    email: '',
+    showPassword: false,
+    rememberMe: true
+  })
+
+  // ** Hook
+  const theme = useTheme()
+
+  const handleChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword })
+  }
+
+  const handleLocalLogin = () => {
+    login(values, console.log)
+  }
+
+  return (
+    <Box className='content-center'>
+      <Card sx={{ zIndex: 1 }}>
+        <CardContent sx={{ p: theme => `${theme.spacing(13, 7, 6.5)} !important` }}>
+          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <img src='/images/logo.png' style={{ maxWidth: '100%' }} />
+          </Box>
+          <Box sx={{ mb: 6, textAlign: 'center' }}>
+            <Typography variant='h5' sx={{ mb: 1.5, fontWeight: 600, letterSpacing: '0.18px' }}>
+              Welcome back!
+            </Typography>
+            <Typography variant='body2'>Please enter your email and password below.</Typography>
+          </Box>
+          <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
+            <TextField autoFocus fullWidth id='email' onChange={handleChange('email')} label='Email' sx={{ mb: 4 }} />
+            <FormControl fullWidth>
+              <InputLabel htmlFor='auth-login-password'>Password</InputLabel>
+              <OutlinedInput
+                label='Password'
+                value={values.password}
+                id='auth-login-password'
+                onChange={handleChange('password')}
+                type={values.showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position='end'>
+                    <IconButton
+                      edge='end'
+                      onClick={handleClickShowPassword}
+                      onMouseDown={e => e.preventDefault()}
+                      aria-label='toggle password visibility'
+                    >
+                      <Icon icon={values.showPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} />
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <Box
+              sx={{ mb: 4, display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-between' }}
+            >
+              <FormControlLabel
+                label='Remember Me'
+                control={<Checkbox />}
+                sx={{ '& .MuiFormControlLabel-label': { color: 'text.primary' } }}
+              />
+              <Typography
+                variant='body2'
+                component={Link}
+                href='/pages/auth/forgot-password-v1'
+                sx={{ color: 'primary.main', textDecoration: 'none' }}
+              >
+                Forgot Password?
+              </Typography>
+            </Box>
+            <Button onClick={handleLocalLogin} fullWidth size='large' type='submit' variant='contained' sx={{ mb: 7 }}>
+              Login
+            </Button>
+            <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+              <Typography sx={{ mr: 2, color: 'text.secondary' }}>New on our platform?</Typography>
+              <Typography
+                component={Link}
+                href='/pages/register'
+                sx={{ color: 'primary.main', textDecoration: 'none' }}
+              >
+                Create an account
+              </Typography>
+            </Box>
+            <Divider
+              sx={{
+                '& .MuiDivider-wrapper': { px: 4 },
+                mt: theme => `${theme.spacing(5)} !important`,
+                mb: theme => `${theme.spacing(7.5)} !important`
+              }}
+            >
+              or
+            </Divider>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <IconButton href='/' component={Link} sx={{ color: '#497ce2' }} onClick={e => e.preventDefault()}>
+                <Icon icon='mdi:facebook' />
+              </IconButton>
+              <IconButton href='/' component={Link} sx={{ color: '#1da1f2' }} onClick={e => e.preventDefault()}>
+                <Icon icon='mdi:twitter' />
+              </IconButton>
+              <IconButton
+                href='/'
+                component={Link}
+                onClick={e => e.preventDefault()}
+                sx={{ color: theme => (theme.palette.mode === 'light' ? '#272727' : 'grey.300') }}
+              >
+                <Icon icon='mdi:github' />
+              </IconButton>
+              <IconButton href='/' component={Link} sx={{ color: '#db4437' }} onClick={e => e.preventDefault()}>
+                <Icon icon='mdi:google' />
+              </IconButton>
+            </Box>
+          </form>
+        </CardContent>
+      </Card>
+      {/* <FooterIllustrationsV1 /> */}
+    </Box>
+  )
+}
+
+LoginV1.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>
+
+LoginV1.guestGuard = true
+
+export default LoginV1
